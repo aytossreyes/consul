a4c5da5002b5e8c21cacb88892f725c9f1174e40
Fernando Blat <ferblape@gmail.com>
2016-12-21 12:57:25 +0100

    Deploy configuration for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,20 @@
+require 'net/ssh/proxy/command'
+
+set :repo_url, 'git@github.com:aytossreyes/consul.git'
 set :deploy_to, deploysecret(:deploy_to)
 set :server_name, deploysecret(:server_name)
 set :db_server, deploysecret(:db_server)
-set :branch, :stable
+set :branch, :master
 set :ssh_options, port: deploysecret(:ssh_port)
 set :stage, :production
 set :rails_env, :production
 
-#server deploysecret(:server1), user: deploysecret(:user), roles: %w(web app db importer)
-server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron)
-server deploysecret(:server3), user: deploysecret(:user), roles: %w(web app db importer)
-server deploysecret(:server4), user: deploysecret(:user), roles: %w(web app db importer)
+server deploysecret(:server), user: deploysecret(:user), roles: %w(web app db importer cron)
+
+set :ssh_options, {
+  proxy: Net::SSH::Proxy::Command.new("ssh -p 22 ubuntu@populate01 -W %h:%p"),
+  forward_agent: true,
+  user: 'ubuntu'
+}
+
+set :pty, false
