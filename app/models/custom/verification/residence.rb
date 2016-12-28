1bde0718678022c6c8f075d1dc4d8bd134ae2658
Fernando Blat <ferblape@gmail.com>
2016-12-28 08:50:26 +0100

    Overwrite residence validation

--- app/models/custom/verification/residence.rb
+++ app/models/custom/verification/residence.rb
@@ -1,29 +1,27 @@
-
 require_dependency Rails.root.join('app', 'models', 'verification', 'residence').to_s
 
 class Verification::Residence
 
-  validate :postal_code_in_madrid
-  validate :residence_in_madrid
+  validate :residence
 
-  def postal_code_in_madrid
-    errors.add(:postal_code, I18n.t('verification.residence.new.error_not_allowed_postal_code')) unless valid_postal_code?
-  end
-
-  def residence_in_madrid
+  def residence
     return if errors.any?
 
     unless residency_valid?
-      errors.add(:residence_in_madrid, false)
+      errors.add(:residence, false)
       store_failed_attempt
       Lock.increase_tries(user)
     end
   end
 
+
   private
 
-    def valid_postal_code?
-      postal_code =~ /^280/
+    # We are overwriting residency check, to avoid checking against the postal code
+    # because the census from San Sebastián de los Reyes doesn't include postal code information
+    def residency_valid?
+      @census_api_response.valid? &&
+        @census_api_response.date_of_birth == date_of_birth
     end
 
 end
