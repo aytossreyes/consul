65920410426387a224df92679158c6d420a5b814
Fernando Blat <ferblape@gmail.com>
2017-01-04 11:12:27 +0100

    Restore defaults

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,20 +1,12 @@
-require 'net/ssh/proxy/command'
-
-set :repo_url, 'git@github.com:aytossreyes/consul.git'
 set :deploy_to, deploysecret(:deploy_to)
 set :server_name, deploysecret(:server_name)
 set :db_server, deploysecret(:db_server)
-set :branch, :master
+set :branch, :stable
 set :ssh_options, port: deploysecret(:ssh_port)
 set :stage, :production
 set :rails_env, :production
 
-server deploysecret(:server), user: deploysecret(:user), roles: %w(web app db importer cron)
-
-set :ssh_options, {
-  proxy: Net::SSH::Proxy::Command.new("ssh -p 22 ubuntu@populate01 -W %h:%p"),
-  forward_agent: true,
-  user: 'ubuntu'
-}
-
-set :pty, false
+#server deploysecret(:server1), user: deploysecret(:user), roles: %w(web app db importer)
+server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
+server deploysecret(:server3), user: deploysecret(:user), roles: %w(web app db importer)
+server deploysecret(:server4), user: deploysecret(:user), roles: %w(web app db importer)
